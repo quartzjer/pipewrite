4f798c515a4ce95ddc4efe1557cb21444ca98d3a
Jeremie Miller <jeremie@jabber.org>
2013-06-05 16:03:34 -0500

    first push, basically working

---
+++ app.js
@@ -0,0 +1,115 @@
+/*
+* Copyright (C) 2013 Singly, Inc. All Rights Reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above copyright
+*      notice, this list of conditions and the following disclaimer in the
+*      documentation and/or other materials provided with the distribution.
+*    * Neither the name of the Locker Project nor the
+*      names of its contributors may be used to endorse or promote products
+*      derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE LOCKER PROJECT BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+var express = require('express');
+var request = require('request');
+var sprintf = require('sprintf').sprintf;
+var async = require("async");
+
+// The port that this express app will listen on
+var port = process.env.PORT || 7464;
+
+// Your client ID and secret from http://dev.singly.com/apps
+var s3 = new require('./s3').backend({key:process.env.S3_KEY, secret:process.env.S3_SECRET, bucket:process.env.S3_BUCKET});
+
+// Create an HTTP server
+var app = express();
+
+// Setup for the express web framework
+app.configure(function() {
+  app.use(express.logger());
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
+// We want exceptions and stracktraces in development
+app.configure('development', function() {
+  app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+});
+
+// Render out views/index.ejs, passing in the session
+app.get('/', function(req, res) {
+  res.end("nothing to see here, move along");
+});
+
+app.post('/drain/:service/:user', function(req, res){
+  if(!Array.isArray(req.body)) {
+    console.log("bad data", req.body);
+    return;
+  }
+  var buckets = {};
+  var adding = {};
+  // dump into per-day buckets
+  function pad(n){return n<10 ? '0'+n : n};
+  req.body.forEach(function(entry){
+    if(!entry || entry.type != "data" || !entry.data || !entry.data.created_at) return;
+    var dayte = new Date(entry.data.created_at);
+    var day = [dayte.getUTCFullYear(), pad(dayte.getUTCMonth()), pad(dayte.getUTCDate())].join("-");
+    if(!buckets[day]) buckets[day] = [];
+    buckets[day].push(entry);
+    adding[entry.entry_id] = true;
+  });
+
+  async.forEach(Object.keys(buckets), function(day, cbDay){
+    var dest = req.params.service+"/"+req.params.user+"/"+day+".json";
+    s3.get(dest, function(err, buf){
+      if(err) return cbDay(err);
+      var existing = [];
+      if(buf) try{ existing = JSON.parse(buf) } catch(E){ console.log("couldn't parse", dest, buf.toString()); };
+      // skip any being added again
+      existing.forEach(function(entry){ if(!adding[entry.entry_id]) buckets[day].push(entry); });
+      s3.put(dest, new Buffer(JSON.stringify(buckets[day])), cbDay);
+    });
+  }, function(err){
+    if(err) {
+      console.log("failed",err);
+      return res.send(500)      
+    }
+
+    var index = {};
+    Object.keys(buckets).forEach(function(day){
+      index[day] = buckets[day].length;
+    })
+    res.send(200);
+    console.log("Saved", JSON.stringify(index));
+
+    // update index
+    var dest = req.params.service+"/"+req.params.user+"/index.json";
+    s3.get(dest, function(err, buf){
+      var existing = {};
+      if(buf) try{ existing = JSON.parse(buf) } catch(E){ console.log("couldn't parse", dest, buf.toString()); };
+      Object.keys(index).forEach(function(day){ existing[day] = index[day]; });
+      s3.put(dest, new Buffer(JSON.stringify(index)), function(err){
+        if(err) console.log("failed to save",dest,err);
+      });
+    })
+  });
+});
+
+
+app.listen(port);
+
+console.log(sprintf('Listening on port %s', port));
---
+++ s3.js
@@ -0,0 +1,111 @@
+/*
+* Copyright (C) 2013 Singly, Inc. All Rights Reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*    * Redistributions of source code must retain the above copyright
+*      notice, this list of conditions and the following disclaimer.
+*    * Redistributions in binary form must reproduce the above copyright
+*      notice, this list of conditions and the following disclaimer in the
+*      documentation and/or other materials provided with the distribution.
+*    * Neither the name of the Locker Project nor the
+*      names of its contributors may be used to endorse or promote products
+*      derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE LOCKER PROJECT BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+var knox = require("knox");
+var zlib = require('compress-buffer');
+
+// ***********************************************
+//
+// S3-based storage with compression
+//
+// ***********************************************
+exports.backend = function (args) {
+
+  // TODO, add one auto-retry on 500
+
+  this.client = knox.createClient(args);
+
+  this.get = function (key, cbDone) {
+    var startTime = Date.now();
+    var headers = {"Content-Type": "x-json/gz"};
+    var req = this.client.get(key, headers);
+
+    req.on("response", function (res) {
+
+      // Dump debugging info about response
+      //
+      // TODO: Should logger.debug be efficient and let us avoid this additional
+      // if statement?
+      if (args.debug) {
+        args.debug("S3 GET status:" + res.statusCode + " key:" + key);
+        args.debug(res.headers);
+      }
+
+      if (res.statusCode === 200) {
+        var buffer = new Buffer(0);
+
+        res.on("data", function (chunk) {
+          buffer = Buffer.concat([buffer, chunk]);
+        });
+
+        res.on("end", function () {
+          cbDone(null, zlib.uncompress(buffer));
+        });
+      } else if(res.statusCode === 404){
+        cbDone();
+      } else {
+        var msg = "";
+        res.on("data", function (data) { msg += data.toString(); });
+        res.on("end", function () {
+          cbDone(new Error("S3 GET error: " + res.statusCode + " key:" + key + " " + msg.toString()), null);
+        });
+      }
+    });
+
+    // Execute the request
+    return req.end();
+  };
+
+  this.put = function (key, buffer, cbDone) {
+    buffer = zlib.compress(buffer);
+    var req = this.client.put(key, {
+      "Content-Length": buffer.length,
+      "Content-Type": "x-json/gz",
+      "x-amz-acl": "private"
+    });
+
+    req.on("response", function (res) {
+      // Dump debugging info about response
+      if (args.debug) {
+        args.debug("S3 PUT status:" + res.statusCode + " key:" + key);
+        args.debug(res.headers);
+      }
+
+      if (res.statusCode === 200) {
+        cbDone(null);
+      } else {
+        var msg = "";
+        res.on("data", function (data) { msg += data.toString(); });
+        res.on("end", function () {
+          cbDone(new Error("S3 PUT error: " + res.statusCode + " key: " + key + " " + msg.toString()));
+        });
+      }
+    });
+    req.end(buffer);
+  };
+
+  return this;
+};
